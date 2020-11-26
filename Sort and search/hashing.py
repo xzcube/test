455256e9473d710babed2b680c98634b74c62544
xzcube <1482240153@qq.com>
2020-11-26 23:40:37 +0800

    散列

---
+++ Sort and search/hashing.py
@@ -0,0 +1,89 @@
+import hashlib
+
+m = hashlib.sha1("hellow word".encode("utf-8"))
+# print(m.hexdigest())
+
+
+n = hashlib.md5()
+n.update("hellow word".encode("utf-8"))
+n.update("part 1".encode("utf-8"))
+n.update("part 2".encode("utf-8"))
+# print(n.hexdigest())
+
+
+def hash(astring, tablesize):
+    sum = 0
+    for pos in range(len(astring)):
+        sum += pos * ord(astring[i])
+    return sum % tablesize
+
+# print(hash("cat", 14))
+
+
+class HashTable:
+    def __init__(self, size=11):
+        self.size = size
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self, key, data):
+        hashvalue = self.hashfunction(key, len(self.slots))
+
+        if self.slots[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
+        else:
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data
+            else:
+                nextslot = self.rehash(hashvalue, len(self.slots))
+                while self.slots[nextslot] != None and self.slots[nextslot] != key:
+                    nextslot = self.rehash(nextslot, len(self.slots))
+                if self.slots[nextslot] == None:
+                    self.slots[nextslot] = key
+                    self.data[nextslot] = data
+                else:
+                    self.data[nextslot] = data
+
+    def hashfunction(self, key, size):
+        return key % size
+
+    def rehash(self, oldhash, size):
+        return (oldhash + 1) % size
+
+    def get(self, key):
+        startslot = self.hashfunction(key, len(self.slots))
+        data = None
+        stop = False
+        found = False
+        position = startslot
+        while self.slots[position] != None and not found and not stop:
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+            else:
+                position = self.rehash(position, len(self.slots))
+                if position == startslot:
+                    stop = True
+        return data
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
+
+
+H = HashTable()
+H[54] = "cat"
+H[26] = "dog"
+H[17] = "tiger"
+H[77] = "bird"
+H[31] = "cow"
+H[44] = "goat"
+H[55] = "pig"
+H[20] = "chicken"
+print(H.slots)
+print(H[20])
+H[20] = "duck"
+print(H.data)
